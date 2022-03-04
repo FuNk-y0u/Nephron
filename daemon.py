a1e6293d1a88ace2fe85dbe5accc7a05df13f406
fy-st0rm <slokpradhan2062@gmail.com>
2022-03-04 19:23:28 +0545

    Prototype 1.0

--- daemon.py
+++ daemon.py
@@ -1,3 +1,118 @@
+
+# Host ngrok server
+# Send the ip to server to provide to other users
+# Then host the socket server
+
 import socket
+import time
+import random
+import threading
+import pickle
+from pyngrok import ngrok
 
+SV_IP	= "127.0.0.1"
+SV_PORT = 5050
+SV_ADDR = (SV_IP, SV_PORT)
 
+CLIENT_IP = "127.0.0.1"
+CLIENT_PORT = random.randint(1000, 9999) 
+CLIENT_ADDR = (CLIENT_IP, CLIENT_PORT)
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.connect(SV_ADDR)
+
+BUFFER = 1024
+FORMAT = "utf-8"
+
+# TOKENS
+DATA = "data"
+PACKET = "packet"
+PUSH = "push"
+PULL = "pull"
+CLIENTS = "clients"
+
+# User id
+user_id = int(server.recv(BUFFER).decode(FORMAT))
+
+# Starting a ngrok 
+tcp_tunnel = ngrok.connect(CLIENT_PORT, "tcp")
+url = tcp_tunnel.public_url
+split = url.split(":")
+host_url = split[1].split("//")[1]
+
+# Getting ip and port
+ip = socket.gethostbyname(host_url)
+port = split[2]
+cli_addr = {"ip": ip, "port": port}
+server.send(pickle.dumps(cli_addr))
+
+# Our hosted server
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.bind(CLIENT_ADDR)
+
+# All the online users ip and port
+online_users = []
+peers_threads = []
+
+# Packets
+packets = []
+
+# Parser
+def packet_parser():
+	print(packets)
+	while True:
+		for i in packets:
+			if CLIENTS in i:
+				clients = i[CLIENTS]
+				online_users.clear()
+				for j in clients:
+					online_users.append(clients[j])
+				packets.remove(i)
+
+# Listeners
+def server_listener():
+	while True:
+		data = server.recv(BUFFER)
+		if data:
+			packets.append(pickle.loads(data))
+
+def peer_listener(idx):
+	addr = online_users[idx]
+	peer = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	peer.connect((addr["ip"], addr["port"]))
+
+	while True:
+		data = peer.recv(BUFFER)
+		if data:
+			packets.append(pickle.loads(data))
+		else:
+			break
+
+def peer_send(packet):
+	if len(online_users) > 1:
+		addr = random.choice(online_users)	
+		if addr["port"] != CLIENT_PORT:
+			peer = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+			peer.connect((addr["ip"], addr["port"]))
+			print(addr, packet)
+			peer.send(pickle.dumps(packet))
+			peer.close()
+
+def run_listeners():
+	for idx in range(1, len(online_users)):
+		thread = threading.Thread(target = peer_listener, args = (idx,))
+		thread.start()
+		peers_threads.append(thread)
+
+if __name__ == "__main__":
+	sv_listen_thread = threading.Thread(target = server_listener)
+	sv_listen_thread.start()
+
+	parser_thread = threading.Thread(target = packet_parser)
+	parser_thread.start()
+
+	run_listeners()
+	packet = {"Helo": "World"}
+	while True:
+		peer_send(packet)
+		#print(packets)
--- server/server.py
+++ server/server.py
@@ -1,41 +1,76 @@
 import socket
+import pickle
 import threading
+import time
 
 IP	 = "127.0.0.1"
 PORT = 5050
+BUFFER = 1024
+FORMAT = "utf-8"
+
+# TOKENS
+DATA = "data"
+PACKET = "packet"
+PUSH = "push"
+PULL = "pull"
+CLIENTS = "clients"
 
 class Server:
 	def __init__(self):
 		self.__create_server()
 		self.running = True
-		self.online_users = []	# TODO: Maybe change to dictionary when usernames are indroduced
+		self.user_offset = 0
+		self.online_cons  = []
+		self.online_users = {CLIENTS: {}} # TODO: Maybe change to dictionary when usernames are indroduced
 	
 	def __create_server(self):
 		self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 		self.server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 		self.server.bind((IP, PORT))
 
-	def __handle_client(self, conn):
+	def __handle_client(self, user_offset, conn, addr):
 		while True:
-			on_user = " ".join(f"{i}" for i in self.online_users)
+			time.sleep(0.5)
 			try:
-				conn.send(on_user)
-			except socket.error:
-				self.online_users.remove(conn)
+				data = conn.recv(BUFFER)
+				if not data:
+					self.online_users[CLIENTS].pop(user_offset)
+					self.online_cons.remove(conn)
+					for i in self.online_cons:
+						i.send(pickle.dumps(self.online_users))
+					break
+			except ConnectionResetError:
+				self.online_users[CLIENTS].pop(user_offset)
+				self.online_cons.remove(conn)
+				for i in self.online_cons:
+					i.send(pickle.dumps(self.online_users))
 				break
+
 		conn.close()
 	
 	def run(self):
 		self.server.listen()
-		print(f"Server listening in {self.ip}")
+		print(f"Server listening in {IP}")
 		
 		while self.running:
 			conn, addr = self.server.accept()
-			# Addding the new user to group
-			self.online_users.append(conn);
 
-			new_thread = threading.Thread(target=self.__handle_client, args=(conn, ))
+			# Addding the new user to group
+			self.online_cons.append(conn)
+
+			conn.send(f"{self.user_offset}".encode(FORMAT))
+			client_addr = pickle.loads(conn.recv(BUFFER))
+
+			self.online_users[CLIENTS].update({self.user_offset: client_addr})
+			for i in self.online_cons:
+				i.send(pickle.dumps(self.online_users))
+
+			print(self.online_users)
+
+			new_thread = threading.Thread(target=self.__handle_client, args=(self.user_offset, conn, addr))
 			new_thread.start()
+			self.user_offset += 1
 
 if __name__ == "__main__":
-		
+	server = Server()
+	server.run()
